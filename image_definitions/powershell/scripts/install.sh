a25bb5c5d2007f2604ab27fa91eaf82e3bf8a484
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-11-29 14:38:24 +0000

    Updating to use Ubuntu
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ image_definitions/powershell/scripts/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the ARCH of the enviornment
+ARCH="$(uname -m)"
+if [[ ${ARCH} == "x86_64" ]];
+then
+    ARCH="amd64"
+elif [[ ${ARCH} == "aarch64" ]];
+then
+    ARCH="arm64"
+fi
+
+# Download the PowerShell install file
+URL="https://github.com/PowerShell/PowerShell/releases/download/v${POWERSHELL_VERSION}/powershell_${POWERSHELL_VERSION}-1.deb_${ARCH}.deb"
+echo "Downloading: ${URL}"
+curl -L $URL -o /tmp/powershell.deb && \
+
+# Install the package
+dpkg -i /tmp/powershell.deb
+
+# Install any missing dependencies
+apt-get install -y -f
+
+# Remove the package
+rm /tmp/powershell.deb
