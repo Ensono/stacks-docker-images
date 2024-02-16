18ce554b46a9467104eab731b3d1cdd91c60e9c6
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-02-16 14:48:53 +0000

    Added infrastructure for ACR Updated to create multiplatform images
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ deploy/terraform/azure/acr/outputs.tf
@@ -0,0 +1,28 @@
+output "resource_group_name" {
+  description = "Name of the resource group for the anciallry resources"
+  value       = azurerm_resource_group.rg.name
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+output "acr_admin_username" {
+  description = "Admin username for the container registry"
+  value = azurerm_container_registry.registry.admin_username
+}
+
+output "acr_admin_password" {
+  description = "Admin password for the container registry"
+  value = azurerm_container_registry.registry.admin_password
+  sensitive = true
+}
+
+output "acr_name" {
+  description = "Name of the created container registry"
+  value = azurerm_container_registry.registry.name
+}
+
+output "acr_url" {
+  description = "URL of the container registry"
+  value = azurerm_container_registry.registry.login_server
+}
