bdc8a70a8183d9fd670ebdcc44c339ffe06fad57
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-04-17 16:45:16 +0100

    Updated documentation Moved AsciiDoctor library into AsciiDoctor  image
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- src/definitions/asciidoctor/files/lib/glob-include-processor.rb
+++ src/definitions/asciidoctor/files/lib/glob-include-processor.rb
@@ -1,3 +1,6 @@
+# Library declaration that allows files to be included in a document using a wildcard, rather
+# than having to include the files individually
+
 RUBY_ENGINE == 'opal' ? (require 'glob-include-processor/extension') : (require_relative 'glob-include-processor/extension')
 
 Asciidoctor::Extensions.register do
