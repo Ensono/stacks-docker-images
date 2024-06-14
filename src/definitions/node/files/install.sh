67ba93ffdff8d4bcf24e099c3db1be2a07dbfd61
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-06-14 14:07:36 +0100

    Fixed issue with Node image
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- src/definitions/node/files/install.sh
+++ src/definitions/node/files/install.sh
@@ -2,6 +2,13 @@
 
 # Download and install the specified version of NVM
 curl -L -o /tmp/nvm_install https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh 
+chmod +x /tmp/nvm_install
+/tmp/nvm_install
+
+# Activate NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Iterate around the versions of Node required
 versions=$(echo ${NODE_VERSIONS} | tr "," "\n")
