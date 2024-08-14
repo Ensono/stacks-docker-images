b28ce1894116028b80c492bef32c9b8e91f247ba
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-08-14 14:47:16 +0100

    Updated profile with variables to assist with output from the EIR Updated documentation

--- src/definitions/foundation/powershell/files/Microsoft.PowerShell_profile.ps1
+++ src/definitions/foundation/powershell/files/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,14 @@ if ($env:PSModulePath -notlike "*:/modules*")
 {
     $env:PSModulePath += ":/modules"
 }
+
+# Set the information preference so that Write-Information is always displayed
+$InformationPreference = "Continue"
+
+# Set the ErrorAction so that if there is an error it will stop
+$ErrorActionPreference = "Stop"
+
+# Allow errors from CLI commands to be treated more like PowerShell errors
+# This is available in PowerShell 7,3 and later
+# https://matthorgan.xyz/blog/powershell-handling-native-applications/
+$PSNativeCommandErrorActionPreference = $true
